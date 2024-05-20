699d4e12670262d8d4f1f561a4d5a10558f973c4
Anthony <anthonyassayah14@gmail.com>
2024-05-20 16:23:29 +0300

    gitignore

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-
   const url = "http://localhost:4000";
 
   return (
@@ -21,9 +19,9 @@ const App = () => {
       <div className="app-content">
         <Sidebar />
         <Routes>
-        <Route path="/add" element={<Add url={url} />} />
-        <Route path="/list" element={<List url={url} />} />
-        <Route path="/orders" element={<Orders url={url} />} />
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
