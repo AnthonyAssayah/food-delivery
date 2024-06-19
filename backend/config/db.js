c7df2fcfec18023d569e9a06e5e44530db4fc080
Anthony <anthonyassayah14@gmail.com>
2024-06-19 19:48:03 +0300

    update mongodb url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://anthonyassayah14:sSsDRBF0UDzHCZx6@cluster0.cqv8tum.mongodb.net/food-del')
+    await mongoose.connect('mongodb+srv://anthonyassayah14:sSsDRBF0UDzHCZx6@cluster0.cqv8tum.mongodb.net/food-del?retryWrites=true&w=majority&appName=Cluster0')
     .then(()=> console.log("db connected"));
 };
